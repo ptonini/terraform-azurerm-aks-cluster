38cec97f3bfa3c4f74112cb29da2499cdcfd6975
ptonini <pedro.tonini@hotmail.com>
2023-12-13 09:44:53 -0300

    feat: added kube_config outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,15 @@ output "this" {
   value = azurerm_kubernetes_cluster.this
 }
 
+output "kube_config" {
+  value = coalesce(one(azurerm_kubernetes_cluster.this.kube_admin_config[*]), one(azurerm_kubernetes_cluster.this.kube_config[*]))
+}
+
+output "kube_config_raw" {
+  sensitive = true
+  value = coalesce(one(azurerm_kubernetes_cluster.this.kube_admin_config_raw[*]), one(azurerm_kubernetes_cluster.this.kube_config_raw[*]))
+}
+
 output "identities" {
   value = { for k, v in module.identities : k => v.this }
 }
@@ -9,9 +18,8 @@ output "identities" {
 output "identity_helm_release_values" {
   value = { for k, v in module.identities : k => {
     pod = { labels = { "azure.workload.identity/use" = "true" } }
-    serviceaccount = {
-      enabled     = true
-      annotations = { "azure.workload.identity/client-id" = v.this.client_id }
+    serviceAccount = {
+      annotations = { "azure.workload.identity/client-id" = v.this["client_id"] }
     }
   } }
 }
